7861002b7408f3dce84ae23eff8e889a5acaffa2
Sarah Steininger <sarahsteininger@Sarahs-MacBook-Pro.local>
2024-03-14 14:09:07 +0100

    removed unused files

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO vehicle (brand, model) VALUES ('Lamborghini', 'Aventador');
-INSERT INTO vehicle (brand, model) VALUES ('Ferrari', 'Bburago');
-INSERT INTO vehicle (brand, model) VALUES ('Audi', 'RS');
